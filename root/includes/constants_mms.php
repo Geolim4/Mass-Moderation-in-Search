454a46f28f2e8c33c511d9f40d17e3fc7e1d3a5f
Geolim4 <contact@geolim4.com>
2014-03-17 18:07:50 +0100

    Wrong jqueryUI update

---
+++ root/includes/constants_mms.php
@@ -0,0 +1,20 @@
+<?php
+/**
+*
+* @package phpBB3 Mass Moderation in Search Constants
+^>@version $Id: constants_mms.php v1.1.1 07h79 03/16/2014 Geolim4 Exp $
+* @copyright (c) 2013 Geolim4.com  http://Geolim4.com
+* @bug/function request: http://geolim4.com/tracker.php
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*/
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+define('MMS_DELAYED_REDIRECT', 1);
+define('MMS_INSTANT_REDIRECT', 2);
